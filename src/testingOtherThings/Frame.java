a19ebaf5bcfa3aa396d0e9975973546c2ac567ee
k00770 <k00770@E4-20.readingschool.reading.sch.uk>
2016-09-12 10:36:00 +0100

    Working on gunfire mechanics
    - Add a strobe effect on the gunfire

---
+++ src/testingOtherThings/Frame.java
@@ -0,0 +1,21 @@
+package testingOtherThings;
+
+import javax.swing.JFrame;
+
+public class Frame {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+
+		Main m = new Main();
+
+		f.setTitle("AI Testing");
+		f.setSize(1920, 1080);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(m);
+		// f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+	}
+
+}
---
+++ src/testingOtherThings/Main.java
@@ -0,0 +1,89 @@
+package testingOtherThings;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Main extends JPanel implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	Timer t = new Timer(5, this);
+
+	int vx = 0, vy = 1;
+
+	Rectangle aiRect = new Rectangle(50, 100, 30, 30);
+
+	Rectangle lineHor = new Rectangle(aiRect.x + 15 - 2000, aiRect.y + 15, 4000, 1);
+	Rectangle lineVer = new Rectangle(aiRect.x + 15, aiRect.y + 15 - 1000, 1, 2000);
+
+	Rectangle[] hitRect = new Rectangle[5];
+
+	Rectangle[] bullet = new Rectangle[4];
+
+	public Main() {
+		setFocusable(true);
+
+		t.start();
+
+		hitRect[0] = new Rectangle(160, 530, 40, 30);
+		
+		bullet[0] = new Rectangle(2000, 2000, 1920, 1);
+		bullet[1] = new Rectangle(aiRect.x - 1950, aiRect.y, 1920, 1);
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponents(g);
+
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 1920, 1080);
+
+		g.setColor(Color.BLUE);
+		g.fillRect(aiRect.x, aiRect.y, aiRect.width, aiRect.height);
+
+		g.setColor(Color.RED);
+		g.fillRect(hitRect[0].x, hitRect[0].y, hitRect[0].width,
+				hitRect[0].height);
+
+		g.fillRect(lineHor.x, lineHor.y, lineHor.width, lineHor.height);
+		g.fillRect(lineVer.x, lineVer.y, lineVer.width, lineVer.height);
+		
+		g.setColor(Color.YELLOW);
+		g.fillRect(bullet[0].x, bullet[0].y, bullet[0].width, bullet[0].height);
+
+		if (lineHor.intersects(hitRect[0]) || lineVer.intersects(hitRect[0])) {
+			
+			bullet[0].x = aiRect.x + 30;
+			bullet[0].y = aiRect.y + 30;
+			bullet[0].width = hitRect[0].x - bullet[0].x;
+
+		} else{
+			bullet[0].x = 2000;
+			bullet[0].y = 2000;
+		}
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		aiRect.x += vx;
+		aiRect.y += vy;
+		lineHor.x += vx;
+		lineHor.y += vy;
+		lineVer.x += vx;
+		lineVer.y += vy;
+
+		repaint();
+
+	}
+
+}
