6515ab1acce3f3cc0f95b10e0fb48e302e3f881a
Harnaam Jandoo <hjandoo@googlemail.com>
2016-09-30 08:20:52 +0100

    Testing out user login functionality

--- src/databaseTesting/Main.java
+++ src/databaseTesting/Main.java
@@ -5,35 +5,42 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		try {
+		Scanner s = new Scanner(System.in);
 
+		System.out.println("Enter Username");
+
+		String u = s.next();
+
+		System.out.println("Enter Password");
+
+		String p = s.next();
+
+		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			
-		}catch (ClassNotFoundException e) {
-			e.printStackTrace();	
-		}
-		try {
 
-			Connection c = DriverManager.getConnection(
-					"jdbc:mysql://127.0.0.1:3306/coursework", "HJandooDB",
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/coursework", "HJandooDB",
 					"kierath123");
 
-			Statement s = c.createStatement();
+			Statement st = c.createStatement();
 
-			ResultSet rs = s.executeQuery("select * from player_statistics;");
-			
+			ResultSet rs = st.executeQuery(
+					"select * from player_statistics where username = '" + u + "' and password = '" + p + "'");
+
 			while (rs.next()) {
-				System.out.println(rs.getInt("id") + " " + rs.getString("username") + " "
-						+ rs.getString("password") + " " + rs.getInt("kills") + " " + rs.getInt("deaths"));
+				System.out.println(rs.getInt("id") + " " + rs.getString("username") + " " + rs.getString("password")
+						+ " " + rs.getInt("kills") + " " + rs.getInt("deaths"));
 			}
-			
-			
-			c.close();
 
 		} catch (SQLException e) {
 			e.printStackTrace();
