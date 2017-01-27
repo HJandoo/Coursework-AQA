717fea49230921704a0c139954a6b34cc7ec990e
k00770 <k00770@D-Lib01.readingschool.reading.sch.uk>
2017-01-27 10:32:31 +0000

    Added SHA-256 to Hash.java
    - Need to implement it to program properly though

--- src/engine/Hash.java
+++ src/engine/Hash.java
@@ -1,6 +1,27 @@
 package engine;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
 public class Hash {
+	
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	
+	public static void runSha(String text) throws Exception {
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+		System.out.println(bytesToHex(hash));
+	}
+	
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
 
 	public static int getHash(String plain) {
 		// This is used to return an integer based on a string and
